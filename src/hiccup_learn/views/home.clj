bb2bf423872c3d44c2962ceab6799f4f0599ea26
dsapoetra <angga.dimassaputra@gmail.com>
2017-07-06 23:10:06 +0700

    another example

--- src/hiccup_learn/views/home.clj
+++ src/hiccup_learn/views/home.clj
@@ -3,16 +3,26 @@
 
 ;;:refer (html5 include-css include-js)
 
-(defn home-page [input input-2]
+(declare neo-home-page)
+
+(def data-from-db
+  ["This is whatever this is"
+      "Lorem yang banyak "
+      "Another whooooaaa"])
+
+(defn home-page [input]
       (hiccup/html5
         [:head
          [:title "Hiccup learn"]]
-
         [:body
          [:div
-          input]
+          (into [:ul]
+                (mapv #(do [:li (str "what the " %)]) (range (read-string input))))]
          [:div
-          input-2]]))
+          [:img {:src "/img/IMG_0400.JPG"}]
+          [:h4 "This is the passage"]
+          (into [:ol] (mapv #(do [:li %]) data-from-db))]]))
+
 
 (defn neo-home-page [input]
       (if (symbol? (read-string input))
@@ -22,3 +32,6 @@
           [:center
            [:h1 {:style "font-size : 99px;"} (str (inc (read-string input)))]])))
 
+
+(defn newthings []
+  (+ 3 3))
